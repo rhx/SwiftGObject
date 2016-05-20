ef347b914aa922f801b99267e07b0e4105510722
Rene Hexel <r.hexel@griffith.edu.au>
2016-05-21 07:27:40 +1000

    Remove redundant include of CGLib

--- Package.swift
+++ Package.swift
@@ -3,7 +3,6 @@ import PackageDescription
 let package = Package(
     name: "GObject",
     dependencies: [
-        .Package(url: "https://github.com/rhx/CGLib.git", majorVersion: 1),
         .Package(url: "https://github.com/rhx/SwiftGLib.git", majorVersion: 2)
     ]
 )
