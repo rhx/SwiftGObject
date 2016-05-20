3fa45dbc15d0a1b889376f860210d83802247c6e
Rene Hexel <r.hexel@griffith.edu.au>
2016-05-21 06:36:26 +1000

    Remove CGLib bindings

--- Package.swift
+++ Package.swift
@@ -3,6 +3,8 @@ import PackageDescription
 let package = Package(
     name: "GObject",
     dependencies: [
-        .Package(url: "https://github.com/rhx/CGlib.git", majorVersion: 1)
+        .Package(url: "https://github.com/rhx/CGLib.git", majorVersion: 1),
+        //.Package(url: "https://github.com/rhx/SwiftGLib.git", majorVersion: 2)
+        .Package(url: "../SwiftGLib", majorVersion: 3)
     ]
 )
