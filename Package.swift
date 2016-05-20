b2d0773c63c8e2cf3cfa09396b1b3f05acf69ddc
Rene Hexel <r.hexel@griffith.edu.au>
2016-05-21 07:11:16 +1000

    Use GitHub repo

--- Package.swift
+++ Package.swift
@@ -3,9 +3,7 @@ import PackageDescription
 let package = Package(
     name: "GObject",
     dependencies: [
-        //.Package(url: "https://github.com/rhx/CGLib.git", majorVersion: 1),
-        .Package(url: "../CGLib", majorVersion: 1),
-        //.Package(url: "https://github.com/rhx/SwiftGLib.git", majorVersion: 2)
-        .Package(url: "../SwiftGLib", majorVersion: 3)
+        .Package(url: "https://github.com/rhx/CGLib.git", majorVersion: 1),
+        .Package(url: "https://github.com/rhx/SwiftGLib.git", majorVersion: 2)
     ]
 )
