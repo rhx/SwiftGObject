0edd3f5f341a5cdc82be110771258d6a78a929a5
Rene Hexel <r.hexel@griffith.edu.au>
2016-06-12 13:41:17 +1000

    Split out config.sh and generate-wrapper.sh from build.sh

--- build.sh
+++ build.sh
@@ -1,39 +1,8 @@
 #!/bin/sh
 #
-MODULE=GObject-2.0
-module=`echo "${MODULE}" | tr '[:upper:]' '[:lower:]'`
-export PATH=/Library/Developer/Toolchains/swift-latest.xctoolchain/usr/bin:"${PATH}"
-GOBJECT_LIBDIR=`pkg-config --libs-only-L gobject-introspection-1.0 2>/dev/null | tr ' ' '\n' | grep gobject-introspection | tail -n1 | cut -c3-`
-GOBJECT_DIR=`dirname "${GOBJECT_LIBDIR}"`
-for prefix in $PREFIX GOBJECT_DIR /usr/local /usr ; do
-	gir_dir=${prefix}/share/gir-1.0
-	gir=${gir_dir}/${MODULE}.gir
-	if [ -e "${gir}" ] ; then
-		export GIR=${gir}
-		export GIR_DIR=${gir_dir}
-	fi
-done
-if [ ! -e "${GIR}" ] ; then
-	echo "*** ${GIR} does not exist!"
-	echo "Make sure libgirepository1.0-dev is installed"
-	echo "and can be found in /usr /usr/local or by pkg-config!"
-	exit 1
-fi
-LINKFLAGS=`pkg-config --libs $module gio-unix-2.0 glib-2.0 | tr ' ' '\n' | sed 's/^/-Xlinker /' | tr '\n' ' '`
-CCFLAGS=`pkg-config --cflags $module gio-unix-2.0 glib-2.0 | tr ' ' '\n' | sed 's/^/-Xcc /' | tr '\n' ' ' `
-gir2swift -p ${GIR_DIR}/GLib-2.0.gir "${GIR}" | sed -f ${MODULE}.sed > Sources/${MODULE}.swift
-echo  > Sources/GObject.swift "import CGLib"
-echo >> Sources/GObject.swift "import GLib"
-echo >> Sources/GObject.swift ""
-grep 'public protocol' Sources/GObject-2.0.swift | cut -d' ' -f3 | cut -d: -f1 | sort -u | sed -e 's/^\(.*\)/public typealias _GObject_\1 = \1/' >> Sources/GObject.swift
-echo >> Sources/GObject.swift ""
-grep 'public class' Sources/GObject-2.0.swift | cut -d' ' -f3 | cut -d: -f1 | sort -u | sed -e 's/^\(.*\)/public typealias _GObject_\1 = \1/' >> Sources/GObject.swift
-echo >> Sources/GObject.swift ""
-echo >> Sources/GObject.swift "public extension GObject {"
-grep 'public protocol' Sources/GObject-2.0.swift | cut -d' ' -f3 | cut -d: -f1 | sort -u | sed -e 's/^\(.*\)/    public typealias \1 = _GObject_\1/' >> Sources/GObject.swift
-echo >> Sources/GObject.swift ""
-grep 'public class' Sources/GObject-2.0.swift | cut -d' ' -f3 | cut -d: -f1 | sort -u | sed -e 's/^\(.*\)/    public typealias \1 = _GObject_\1/' >> Sources/GObject.swift
-echo >> Sources/GObject.swift ""
-grep 'public typealias' Sources/${MODULE}.swift | sed 's/^/    /' >> Sources/GObject.swift
-echo >> Sources/GObject.swift "}"
-swift build $CCFLAGS $LINKFLAGS "$@"
+# Wrapper around `swift build' that uses pkg-config in config.sh
+# to determine compiler and linker flags
+#
+. ./config.sh
+[ -e Sources/${Module}.swift ] || ./generate-wrapper.sh
+exec swift build $CCFLAGS $LINKFLAGS "$@"
