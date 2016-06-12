8b7290410c18ecaa057c75d7a5a7e45e127a15aa
Rene Hexel <r.hexel@griffith.edu.au>
2016-06-12 14:24:43 +1000

    Add some tests

--- Tests/GObject/GObjectTests.swift
+++ Tests/GObject/GObjectTests.swift
@@ -1,18 +1,31 @@
 import XCTest
+import CGLib
 @testable import GObject
 
 class GObjectTests: XCTestCase {
 
-	func testExample() {
-		// This is an example of a functional test case.
-		// Use XCTAssert and related functions to verify your tests produce the correct results.
-	}
+    /// test fundamental types
+    func testTypeFundamental() {
+        let gt: GType = 20
+        let type = type_fundamental(type_id: gt)
+        XCTAssertEqual(type, gt)
+        let name = type_name(type: gt)
+        XCTAssertNotNil(name)
+    }
+
+    /// test whether creating an empty object works
+    func testCreateObject() {
+        var param = GParameter()
+        let object = Object.newv(object_type: 0, n_parameters: 0, parameters: &param)
+        XCTAssertNil(object)
+    }
 
 }
 extension GObjectTests {
-	static var allTests : [(String, GObjectTests -> () throws -> Void)] {
-		return [
-			("testExample", testExample),
-		]
-	}
+    static var allTests : [(String, (GObjectTests) -> () throws -> Void)] {
+        return [
+            ("testTypeFundamental", testTypeFundamental),
+            ("testCreateObject",    testCreateObject),
+        ]
+    }
 }
