be5765c75cbf920df0dc53f92b452f0fbc62fc82
Rene Hexel <r.hexel@griffith.edu.au>
2016-06-20 15:31:30 +1000

    Use a helper function to call closures using GCallback

--- Sources/SwiftGObject.swift
+++ Sources/SwiftGObject.swift
@@ -24,32 +24,31 @@ class ClosureHolder<S,T> {
 /// Internal type for SignalHandler closure holder
 typealias SignalHandlerClosureHolder = ClosureHolder<Void,Void>
 
-/// Internal type for SignalHandler closure holder
-typealias CCallBackHolder = ClosureHolder<(gpointer, gpointer),Void>
-
 /// Convenience extensions for Objects
 public extension ObjectProtocol {
-    /// Connects a (Void) -> Void closure or function to a signal for
-    /// the receiver object.  Similar to g_signal_connect(), but allows
-    /// to provide a Swift closure that can capture its surrounding context.
-    public func connect(signal name: UnsafePointer<gchar>, flags: ConnectFlags = ConnectFlags(0), handler: SignalHandler) -> CUnsignedLong {
-        let h = Unmanaged.passRetained(ClosureHolder(handler))
-        let signalHandler = Unmanaged.passRetained(CCallBackHolder {
-            let ptr = UnsafePointer<SignalHandlerClosureHolder>($1)
-            ptr.pointee.call()
-        })
-        let callback = unsafeBitCast(signalHandler, to: GCallback.self)
-        let swiftHandler = OpaquePointer(bitPattern: h)
-        let rv = signalConnectData(detailedSignal: name, cHandler: callback, data: swiftHandler, destroyData: {
+    /// Connection helper function
+    private func _connect(signal name: UnsafePointer<gchar>, flags: ConnectFlags, data: SignalHandlerClosureHolder, handler: @convention(c) (gpointer, gpointer) -> Void) -> CUnsignedLong {
+        let opaqueHolder = OpaquePointer(bitPattern: Unmanaged.passRetained(data))
+        let callback = unsafeBitCast(handler, to: Callback.self)
+        let rv = signalConnectData(detailedSignal: name, cHandler: callback, data: opaqueHolder, destroyData: {
             if let swift = OpaquePointer($0) {
                 let holder = Unmanaged<SignalHandlerClosureHolder>.fromOpaque(swift)
                 holder.release()
             }
-            if let c = OpaquePointer($1) {
-                let holder = Unmanaged<CCallBackHolder>.fromOpaque(c)
-                holder.release()
-            }
-        }, connectFlags: flags)
+            let _ = $1
+            }, connectFlags: flags)
+        return rv
+    }
+
+    /// Connects a (Void) -> Void closure or function to a signal for
+    /// the receiver object.  Similar to g_signal_connect(), but allows
+    /// to provide a Swift closure that can capture its surrounding context.
+    public func connect(signal name: UnsafePointer<gchar>, flags f: ConnectFlags = ConnectFlags(0), handler: SignalHandler) -> CUnsignedLong {
+        let rv = _connect(signal: name, flags: f, data: ClosureHolder(handler)) {
+            let ptr = OpaquePointer($1)
+            let holder = Unmanaged<SignalHandlerClosureHolder>.fromOpaque(ptr).takeUnretainedValue()
+            holder.call()
+        }
         return rv
     }
 }
