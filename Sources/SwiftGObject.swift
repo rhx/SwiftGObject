91cc435d17bdb67c0d506a489702c5ba8747a3ed
Rene Hexel <r.hexel@griffith.edu.au>
2016-06-24 19:10:36 +1000

    Add swiftier bindings

--- Sources/SwiftGObject.swift
+++ Sources/SwiftGObject.swift
@@ -11,6 +11,10 @@ import GLib
 /// A Void closure to use as a signal handler, that takes no parameters.
 public typealias SignalHandler = () -> ()
 
+/// A conversion closure transforming the first value into the second
+/// This closure needs to return true if successful, or false otherwise
+public typealias ValueTransformer = (ValueRef, ValueRef) -> Bool
+
 /// Internal Class that wraps a closure to make sure the closure is retained
 /// until no longer required
 class ClosureHolder<S,T> {
@@ -21,12 +25,27 @@ class ClosureHolder<S,T> {
     }
 }
 
+/// Internal Class that wraps a closure to make sure the closure is retained
+/// until no longer required
+class BindingHolder<S,T> {
+    let transform_from: (S, T) -> Bool
+    let transform_to:   (T, S) -> Bool
+
+    init(_ transform_from: (S, T) -> Bool, _ transform_to: (T, S) -> Bool) {
+        self.transform_from = transform_from
+        self.transform_to   = transform_to
+    }
+}
+
 /// Internal type for SignalHandler closure holder
 typealias SignalHandlerClosureHolder = ClosureHolder<Void,Void>
 
+/// Internal type for a binding closure holder
+typealias BindingClosureHolder = BindingHolder<ValueRef, ValueRef>
+
 /// Convenience extensions for Objects
 public extension ObjectProtocol {
-    /// Connection helper function
+    /// Connection helper function for signal handler closure
     private func _connect(signal name: UnsafePointer<gchar>, flags: ConnectFlags, data: SignalHandlerClosureHolder, handler: @convention(c) (gpointer, gpointer) -> Void) -> CUnsignedLong {
         let opaqueHolder = OpaquePointer(bitPattern: Unmanaged.passRetained(data))
         let callback = unsafeBitCast(handler, to: Callback.self)
@@ -40,6 +59,20 @@ public extension ObjectProtocol {
         return rv
     }
 
+    /// Binding helper function for binding closure
+    private func _bind<T: ObjectProtocol>(_ source: UnsafePointer<gchar>, to t: T, _ target_property: UnsafePointer<gchar>, flags f: BindingFlags = .default_, holder: BindingClosureHolder, transformFrom transform_from: @convention(c) (gpointer, gpointer, gpointer, gpointer) -> gboolean, transformTo transform_to: @convention(c) (gpointer, gpointer, gpointer, gpointer) -> gboolean) -> BindingRef! {
+        let opaqueHolder = OpaquePointer(bitPattern: Unmanaged.passRetained(holder))
+        let from = unsafeBitCast(transform_from, to: BindingTransformFunc.self)
+        let to   = unsafeBitCast(transform_to,   to: BindingTransformFunc.self)
+        let rv = bindPropertyFull(sourceProperty: source, target: t, targetProperty: target_property, flags: f, transformTo: to, transformFrom: from, userData: opaqueHolder) {
+            if let swift = OpaquePointer($0) {
+                let holder = Unmanaged<BindingClosureHolder>.fromOpaque(swift)
+                holder.release()
+            }
+        }
+        return rv.map { BindingRef($0) }
+    }
+
     /// Connects a (Void) -> Void closure or function to a signal for
     /// the receiver object.  Similar to g_signal_connect(), but allows
     /// to provide a Swift closure that can capture its surrounding context.
@@ -48,6 +81,63 @@ public extension ObjectProtocol {
             let ptr = OpaquePointer($1)
             let holder = Unmanaged<SignalHandlerClosureHolder>.fromOpaque(ptr).takeUnretainedValue()
             holder.call()
+        }
+        return rv
+    }
+
+    /// Creates a binding between @source_property on @source and @target_property
+    /// on @target. Whenever the @source_property is changed the @target_property is
+    /// updated using the same value. For instance:
+    ///
+    /// |[
+    ///   g_object_bind_property (action, "active", widget, "sensitive", 0);
+    /// ]|
+    ///
+    /// Will result in the "sensitive" property of the widget #GObject instance to be
+    /// updated with the same value of the "active" property of the action #GObject
+    /// instance.
+    ///
+    /// If @flags contains %G_BINDING_BIDIRECTIONAL then the binding will be mutual:
+    /// if @target_property on @target changes then the @source_property on @source
+    /// will be updated as well.
+    ///
+    /// The binding will automatically be removed when either the @source or the
+    /// @target instances are finalized. To remove the binding without affecting the
+    /// @source and the @target you can just call g_object_unref() on the returned
+    /// #GBinding instance.
+    ///
+    /// A #GObject can have multiple bindings.
+    public func bind<T: ObjectProtocol>(sourceProperty source_property: UnsafePointer<gchar>, target: T, targetProperty target_property: UnsafePointer<gchar>, flags: BindingFlags = .default_) -> BindingRef! {
+        let rv = g_object_bind_property(ptr, source_property, target.ptr, target_property, flags)
+        return rv.map { BindingRef(opaquePointer: $0) }
+    }
+
+    /// Complete version of bind().
+    ///
+    /// Creates a binding between @source_property on @source and @target_property
+    /// on @target, allowing you to set the transformation functions to be used by
+    /// the binding.
+    ///
+    /// If @flags contains %G_BINDING_BIDIRECTIONAL then the binding will be mutual:
+    /// if @target_property on @target changes then the @source_property on @source
+    /// will be updated as well. The @transform_from function is only used in case
+    /// of bidirectional bindings, otherwise it will be ignored
+    ///
+    /// The binding will automatically be removed when either the @source or the
+    /// @target instances are finalized. To remove the binding without affecting the
+    /// @source and the @target you can just call g_object_unref() on the returned
+    /// #GBinding instance.
+    ///
+    /// A #GObject can have multiple bindings.
+    public func bind<T: ObjectProtocol>(_ source_property: UnsafePointer<gchar>, to target: T, _ target_property: UnsafePointer<gchar>, flags f: BindingFlags = .default_, transformFrom transform_from: ValueTransformer = { $0.transform(destValue: $1) }, transformTo transform_to: ValueTransformer) -> BindingRef! {
+        let rv = _bind(source_property, to: target, target_property, flags: f, holder: BindingClosureHolder(transform_from, transform_to), transformFrom: {
+            let ptr = OpaquePointer($3)
+            let holder = Unmanaged<BindingClosureHolder>.fromOpaque(ptr).takeUnretainedValue()
+            return holder.transform_from(ValueRef(cPointer: $1), ValueRef(cPointer: $2)) ? 1 : 0
+        }) {
+            let ptr = OpaquePointer($3)
+            let holder = Unmanaged<BindingClosureHolder>.fromOpaque(ptr).takeUnretainedValue()
+            return holder.transform_to(ValueRef(cPointer: $1), ValueRef(cPointer: $2)) ? 1 : 0
         }
         return rv
     }
