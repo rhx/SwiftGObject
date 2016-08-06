cbf4ca2189e0e8e762f78788e8de7619a7e92368
Rene Hexel <r.hexel@griffith.edu.au>
2016-08-06 13:25:02 +1000

    Use tac / tail -r for parallel builds

--- config.sh
+++ config.sh
@@ -9,3 +9,10 @@ module="`echo "${Module}" | tr '[:upper:]' '[:lower:]'`"
 mod="`echo "${Mod}" | tr '[:upper:]' '[:lower:]'`"
 LINKFLAGS="`pkg-config --libs $module gio-unix-2.0 glib-2.0 | tr ' ' '\n' | sed 's/^/-Xlinker /' | tr '\n' ' '`"
 CCFLAGS="`pkg-config --cflags $module gio-unix-2.0 glib-2.0 | tr ' ' '\n' | sed 's/^/-Xcc /' | tr '\n' ' ' `"
+TAC="tail -r"
+if which tac >/dev/null ; then
+   TAC=tac
+   else if which gtac >/dev/null ; then
+	TAC=gtac
+   fi
+fi
