411dfec7c4aecfbf7dfa9a70ea6d5cc654059806
Rene Hexel <r.hexel@griffith.edu.au>
2017-04-29 09:29:42 +1000

    Add lookup table convenience constructor for ParamSpecRef

---
+++ Sources/ParamSpec.swift
@@ -0,0 +1,23 @@
+//
+//  ParamSpec.swift
+//  GtkListBoxDNDDemo
+//
+//  Created by Rene Hexel on 29/4/17.
+//  Copyright © 2017 Rene Hexel.  All rights reserved.
+//
+import CGLib
+import GLib
+
+public extension ParamSpecRef {
+    /// Create a parameter specification reference from a lookup table.
+    ///
+    /// - Parameters:
+    ///   - name: property name to look up
+    ///   - pool: lookup table to consult
+    ///   - ownerType: type of the property owner (defaults to `.object`)
+    ///   - walkAncestors: `false` to avoid checking ancestors (defaults to `true`)
+    public init?<P: PropertyNameProtocol>(name: P, from pool: UnsafeMutablePointer<GParamSpecPool>!, ownerType: GType = .object, walkAncestors: Bool = true) {
+        guard let p = g_param_spec_pool_lookup(OpaquePointer(pool), name.rawValue, ownerType, gboolean(walkAncestors ? 1 : 0)) else { return nil }
+        self.init(p)
+    }
+}
