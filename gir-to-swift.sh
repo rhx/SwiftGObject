65f1ba54761cd9f540a39dcba93d688ecade3974
Rene Hexel <r.hexel@griffith.edu.au>
2017-05-14 07:59:56 +1000

    Create one file per type

--- gir-to-swift.sh
+++ gir-to-swift.sh
@@ -21,7 +21,7 @@ if [ ! -e "${GIR}" ] ; then
 	echo "and can be found in /usr /usr/local or by pkg-config!"
 	exit 1
 fi
-gir2swift -o Sources -m ${Module}.module -p ${GIR_DIR}/GLib-2.0.gir "${GIR}"
+gir2swift -o Sources -s -m ${Module}.module -p ${GIR_DIR}/GLib-2.0.gir "${GIR}"
 for src in Sources/*-*.swift ; do
 	sed -f ${Module}.sed < ${src} | awk -f ${Module}.awk > ${src}.out
 	mv ${src}.out ${src}
