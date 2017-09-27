9da6eaeee923d52ef9ecd0e50949f85dbf3db82c
Rene Hexel <r.hexel@griffith.edu.au>
2017-09-28 06:28:51 +1000

    Compile with SwiftLanguageVersion 3 under Swift 4

--- Package.swift
+++ Package.swift
@@ -4,5 +4,6 @@ let package = Package(
     name: "GLibObject",
     dependencies: [
         .Package(url: "https://github.com/rhx/SwiftGLib.git", majorVersion: 2)
-    ]
+    ],
+    swiftLanguageVersions: [3]
 )
