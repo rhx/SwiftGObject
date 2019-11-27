55aa1a51e86328c69df1bf780c1f62170de29950
Rene Hexel <r.hexel@griffith.edu.au>
2019-11-27 14:19:48 +1000

    Add BUILD_DIR support

--- config.sh
+++ config.sh
@@ -4,13 +4,19 @@
 # the compiler and linker flags to use.
 #
 VER=2.0
-JAZZY_VER=2.62.1
+JAZZY_VER=2.62.3
 Mod=`grep name: Package.swift | head -n1 | cut -d'"' -f2`
 Pkg=GObject
 Module=${Pkg}-${VER}
 module="`echo "${Module}" | tr '[:upper:]' '[:lower:]'`"
 mod="`echo "${Pkg}" | tr '[:upper:]' '[:lower:]'`"
-BUILD_DIR=`pwd`/.build
+if [ -z "$BUILD_DIR" ]; then
+  if `pwd -P | grep -q Dropbox` ; then
+    export BUILD_DIR="/tmp/.build-$Module"
+  else
+    export BUILD_DIR=`pwd`/.build
+  fi
+fi
 export PKG_CONFIG_PATH=/usr/local/opt/libffi/lib/pkgconfig:${PKG_CONFIG_PATH}
 export PATH="${BUILD_DIR}/gir2swift/.build/release:${BUILD_DIR}/gir2swift/.build/debug:${PATH}:/usr/local/opt/ruby/bin:`echo /usr/local/lib/ruby/gems/*/bin | tr ' ' '\n' | tail -n1`:${PATH}:`echo /var/lib/gems/*/gems/jazzy-*/bin/ | tr ' ' '\n' | tail -n1`:/usr/local/bin"
 LINKFLAGS="`pkg-config --libs $module gio-unix-${VER} glib-${VER} | tr ' ' '\n' | sed -e 's/^/-Xlinker /' -e 's/-Wl,//' | tr '\n' ' ' | sed -e 's/-Xcc[ 	]*-Xlinker/-Xlinker/g' -e 's/-Xcc *$//' -e 's/-Xlinker *$//'`"
