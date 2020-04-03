0177fd2cbc71ae6d5f613238ccef0259c3605c63
Rene Hexel <r.hexel@griffith.edu.au>
2020-04-04 06:36:47 +1000

    Add nullability attributes

--- Sources/GObjectCHelpers/include/glib_value.h
+++ Sources/GObjectCHelpers/include/glib_value.h
@@ -3,4 +3,4 @@
 
 /// Return the base address of the data union stored in the `GValue` instance.
 /// @param gvalue the value to extract the data pointer for
-void *glibobject_value_dataptr(GValue * const gvalue);
+void *glibobject_value_dataptr(GValue * const gvalue) __attribute__((nonnull)) __attribute__((returns_nonnull));
--- Sources/GObjectCHelpers/include/gobject_helpers.h
+++ Sources/GObjectCHelpers/include/gobject_helpers.h
@@ -3,20 +3,20 @@
 
 /// Get the type of a `GObject` instance.
 /// @param gobject the object to get the type for
-GTypeClass *glibobject_get_instance_type(GObject * const gobject);
+GTypeClass *glibobject_get_instance_type(GObject * const gobject)  __attribute__((nonnull)) __attribute__((returns_nonnull));
 
 /// Return whether the given receiver is a `GObject`.
 /// @param gobject pointer to test
-gboolean glibobject_is_object(GObject * const gobject);
+gboolean glibobject_is_object(GObject * const gobject)  __attribute__((nonnull));
 
 /// Get a single property from a `GObject`
 /// @param gobject the object to enquire
 /// @param name the name of the property to retrieve
 /// @param storage the buffer to store the property value in
-void glibobject_get_property(gpointer gobject, const char *name, void *storage);
+void glibobject_get_property(gpointer gobject, const char *name, void *storage)  __attribute__((nonnull));
 
 /// Set a single property on a `GObject`
 /// @param gobject the object to set the property on
 /// @param name the name of the property to set
 /// @param value the property value to set
-void glibobject_set_property(gpointer gobject, const char *name, intptr_t value);
+void glibobject_set_property(gpointer gobject, const char *name, intptr_t value)  __attribute__((nonnull));
