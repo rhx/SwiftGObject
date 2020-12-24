0b6174ac3c9a2d0795b68e6457b58c63a2b434fb
Mikoláš Stuchlík <42004728+mikolasstuchlik@users.noreply.github.com>
2020-12-24 13:00:41 +0100

    Update WeakObject.swift

--- Sources/GLibObject/WeakObject.swift
+++ Sources/GLibObject/WeakObject.swift
@@ -5,28 +5,31 @@ public protocol GWeakCapturing {
     init(raw: UnsafeMutableRawPointer)
 }
 
+
 @propertyWrapper
 public final class GWeak<T: ObjectProtocol & GWeakCapturing> {
 
-    private var storage: UnsafeMutablePointer<gpointer?>
+    private var storage: UnsafeMutablePointer<UnsafeMutablePointer<gpointer?>>
 
     public var wrappedValue: T? {
-        get {
-            storage.pointee.flatMap(T.init(raw:))
-        }
-        set {
-            assign(newValue: newValue)
-        }
+        get { storage.pointee.pointee.flatMap(T.init(raw:)) }
+        set { assign(newValue: newValue) }
     }
 
-    public init(_ wrappedValue: T?) {
+    public var raw: gpointer? {
+        get { storage.pointee.pointee }
+        set { assign(newValue: newValue.flatMap(T.init(raw:))) }
+    }
+
+    public init(wrappedValue: T?) {
         self.storage = UnsafeMutablePointer.allocate(capacity: 1)
+        self.storage.pointee = UnsafeMutablePointer.allocate(capacity: 1)
+        self.storage.pointee.pointee = nil
         assign(newValue: wrappedValue) 
     }
 
-    public init(raw pointer: UnsafeMutableRawPointer?) {
-        self.storage = UnsafeMutablePointer.allocate(capacity: 1)
-        assign(newValue: pointer.flatMap(T.init(raw:))) 
+    public convenience init(raw pointer: UnsafeMutableRawPointer?) {
+        self.init(wrappedValue: pointer.flatMap(T.init(raw:)))
     }
 
     public convenience init(_ object: ObjectProtocol) {
@@ -35,15 +38,15 @@ public final class GWeak<T: ObjectProtocol & GWeakCapturing> {
 
     private func assign(newValue: T?) {
         let current = wrappedValue
-        current?.removeWeakPointer(weakPointerLocation: storage)
-        storage.pointee = newValue?.ptr
-        newValue?.addWeakPointer(weakPointerLocation: storage)
+        current?.removeWeakPointer(weakPointerLocation: storage.pointee)
+        storage.pointee.pointee = newValue?.ptr
+        newValue?.addWeakPointer(weakPointerLocation: storage.pointee)
         
     }
 
     deinit {
         assign(newValue: nil)
+        storage.pointee.deallocate()
         storage.deallocate()
     }
-
 } 
