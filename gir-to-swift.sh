51a072ee87b77be1daf7c2c7e352055f3da51afc
Rene Hexel <r.hexel@griffith.edu.au>
2021-01-09 15:43:37 +1000

    Reinstate original build scripts with updated directory structure

---
+++ gir-to-swift.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+#
+# Swift code wrapper generator for gobject-introspection (.gir) files.
+# This needs an installed `gir2swift' executable (github.com/rhx/gir2swift)
+#
+. ./config.sh
+mkdir -p Sources
+GOBJECT_LIBDIR=`pkg-config --libs-only-L gobject-introspection-1.0 2>/dev/null | tr ' ' '\n' | grep gobject-introspection | tail -n1 | cut -c3-`
+GOBJECT_DIR=`dirname "${GOBJECT_LIBDIR}"`
+for prefix in /usr /usr/local $GOBJECT_DIR $PREFIX ; do
+	gir_dir=${prefix}/share/gir-1.0
+	gir=${gir_dir}/${Module}.gir
+	if [ -e "${gir}" ] ; then
+		export GIR=${gir}
+		export GIR_DIR=${gir_dir}
+	fi
+done
+if [ ! -e "${GIR}" ] ; then
+	echo "*** ${GIR} does not exist!"
+	echo "Make sure libgirepository1.0-dev is installed"
+	echo "and can be found in /usr /usr/local or by pkg-config!"
+	exit 1
+fi
+gir2swift -o Sources/${Mod} -s -m ${Module}.module -p ${GIR_DIR}/GLib-2.0.gir "${GIR}"
+for src in Sources/${Mod}/*-*.swift ; do
+	sed -f ${Module}.sed < ${src} | awk -f ${Module}.awk > ${src}.out
+	mv ${src}.out ${src}
+done
+touch Sources/${Mod}/${Module}.swift
