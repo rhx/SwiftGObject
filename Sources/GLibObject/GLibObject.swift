50198fe2fec827598f03e0e22d0ebd1fbf5c7c75
Rene Hexel <r.hexel@griffith.edu.au>
2023-04-08 06:38:41 +1000

    Remove unnecessary indentations.

--- Sources/GLibObject/GLibObject.swift
+++ Sources/GLibObject/GLibObject.swift
@@ -3,7 +3,7 @@
 //  GObject
 //
 //  Created by Rene Hexel on 20/06/2016.
-//  Copyright © 2016, 2017, 2018, 2019, 2020, 2021 Rene Hexel.  All rights reserved.
+//  Copyright © 2016, 2017, 2018, 2019, 2020, 2021, 2023 Rene Hexel.  All rights reserved.
 //
 import CGLib
 import GLib
@@ -303,39 +303,39 @@ public extension ObjectProtocol {
 @usableFromInline let gfalse: gboolean = 0
 
 public extension GLibObject.ObjectProtocol {
-	/// The swift wrapper for this object.
+    /// The swift wrapper for this object.
     @inlinable var swiftObj: AnyObject? {
-		get {
+        get {
             guard let pointer = getData(key: swiftObjKey) else { return nil }
             return Unmanaged<AnyObject>.fromOpaque(pointer).takeUnretainedValue()
-		}
-		nonmutating set {
-			// Setting swift object to the already existing swiftObj is a no-op, in order to avoid duplicate toggleRefs, which never fire and thus cause reference cycles.
-			guard let newValue = newValue, newValue !== swiftObj else {
-				return
-			}
-			// Get a strong pointer to swiftObj
-			let pointer = Unmanaged<AnyObject>.passRetained(newValue).toOpaque();
-			setData(key: swiftObjKey, data: pointer);
-			// In the majority of cases, swiftObj will be the swift wrapper for this gobject's c implementation. To prevent orphaning, these should be equivalent for memory management purposes; If one is referenced, the other is referenced. A naive way to implement this is to have both strongly reference each other, but this creates a strong reference cycle. Therefore, the wrapper has a strong toggle reference to the gobject, which tells us when there are other references. In this instance, the wrapper should be referenced, so the gobject strongly references it. Otherwise, the gobject weakly references it, allowing it, and thus the gobject, to be released once it is not referenced in swift-space.
-			addToggleRef { (_, selfPointer, lastRef) in
-				let swiftObjPointer = Unmanaged<AnyObject>.fromOpaque(g_object_get_data(selfPointer, swiftObjKey))
-				switch lastRef {
-				case gfalse:
-					// Make the gobject strongly reference the wrapper.
-					_ = swiftObjPointer.retain()
-				case gtrue:
-					// Make the gobject weakly reference the wrapper.
-					swiftObjPointer.release()
-				default:
-					break
-				}
-			}
-			// Release the regular reference so we don't have two references from the wrapper.
-			unref()
-		}
-	}
-	
+        }
+        nonmutating set {
+            // Setting swift object to the already existing swiftObj is a no-op, in order to avoid duplicate toggleRefs, which never fire and thus cause reference cycles.
+            guard let newValue = newValue, newValue !== swiftObj else {
+                return
+            }
+            // Get a strong pointer to swiftObj
+            let pointer = Unmanaged<AnyObject>.passRetained(newValue).toOpaque();
+            setData(key: swiftObjKey, data: pointer);
+            // In the majority of cases, swiftObj will be the swift wrapper for this gobject's c implementation. To prevent orphaning, these should be equivalent for memory management purposes; If one is referenced, the other is referenced. A naive way to implement this is to have both strongly reference each other, but this creates a strong reference cycle. Therefore, the wrapper has a strong toggle reference to the gobject, which tells us when there are other references. In this instance, the wrapper should be referenced, so the gobject strongly references it. Otherwise, the gobject weakly references it, allowing it, and thus the gobject, to be released once it is not referenced in swift-space.
+            addToggleRef { (_, selfPointer, lastRef) in
+                let swiftObjPointer = Unmanaged<AnyObject>.fromOpaque(g_object_get_data(selfPointer, swiftObjKey))
+                switch lastRef {
+                case gfalse:
+                    // Make the gobject strongly reference the wrapper.
+                    _ = swiftObjPointer.retain()
+                case gtrue:
+                    // Make the gobject weakly reference the wrapper.
+                    swiftObjPointer.release()
+                default:
+                    break
+                }
+            }
+            // Release the regular reference so we don't have two references from the wrapper.
+            unref()
+        }
+    }
+
 }
 
 public extension GLibObject.Object {
@@ -407,16 +407,16 @@ public extension GLibObject.Object {
 
     /// Will set this swift instance to be the swiftObj.
     @inlinable func becomeSwiftObj() {
-		swiftObj = self;
-	}
+        swiftObj = self;
+    }
 }
 
 /// Fetches the swift object from the given pointers, if any. Assume pointer is a GObject, so only call this function if this is known.
 @inlinable public func swiftObj(fromRaw raw: UnsafeMutableRawPointer) -> AnyObject? {
-	let objPointer = g_object_get_data(raw.assumingMemoryBound(to: GObject.self), swiftObjKey);
-	if let objPointer = objPointer {
-		return Unmanaged<AnyObject>.fromOpaque(objPointer).takeUnretainedValue();
-	} else {
-		return nil;
-	}
+    let objPointer = g_object_get_data(raw.assumingMemoryBound(to: GObject.self), swiftObjKey);
+    if let objPointer = objPointer {
+        return Unmanaged<AnyObject>.fromOpaque(objPointer).takeUnretainedValue();
+    } else {
+        return nil;
+    }
 }
